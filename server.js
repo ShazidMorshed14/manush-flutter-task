47326b1663095558b2cdb3f2672f3b1fbdcf101a
Shazid Morshedul Haque <67098062+ShazidMorshed14@users.noreply.github.com>
2025-01-21 01:03:55 +0600

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/products");
